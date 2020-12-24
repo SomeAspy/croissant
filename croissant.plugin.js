d6895ba5ca18d256e0b97d509666608fa0ac3f08
Aiden Baker <33640860+SomeAspy@users.noreply.github.com>
2020-12-23 20:30:54 -0500

    Update croissant.plugin.js

--- croissant.plugin.js
+++ croissant.plugin.js
@@ -10,8 +10,11 @@
 module.exports = class Croissant {
 
     start() {
-        document.addEventListener("click", alert("Croissant", "Croissant"))
+        document.addEventListener("click", ()=> alert("Croissant", "Croissant"))
     } // Required function. Called when the plugin is activated (including after reloads)
     stop() {} // Required function. Called when the plugin is deactivated
-
+    getName() { return "Croissant"; }
+    getAuthor() { return "Some_Aspy#9999"; }
+    getDescription() { return "every click notifies you of a croissant"; }
+    getVersion() { return "0.0.1"; }
 }
